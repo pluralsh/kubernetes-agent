26b7c94a12e64d46c6ffb12520684fa7a4c71ece
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2022-02-17 23:23:56 +0000

    Support ARM architecture

--- build/push_multiarch_agentk.sh
+++ build/push_multiarch_agentk.sh
@@ -9,18 +9,20 @@ if [[ -z "$CI_REGISTRY_IMAGE" ]]; then
 fi
 
 if [[ -z "$basetag" ]]; then
-    echo '$0 takes exactly one argument'
+    echo "$0 takes exactly one argument"
     return 1
 fi
 
 docker manifest create "${CI_REGISTRY_IMAGE}/agentk:${basetag}" \
     --amend "${CI_REGISTRY_IMAGE}/agentk:${basetag}-amd64" \
+    --amend "${CI_REGISTRY_IMAGE}/agentk:${basetag}-arm" \
     --amend "${CI_REGISTRY_IMAGE}/agentk:${basetag}-arm64"
 
 docker manifest push "${CI_REGISTRY_IMAGE}/agentk:${basetag}"
 
 docker manifest create "${CI_REGISTRY_IMAGE}/agentk:${basetag}-race" \
     --amend "${CI_REGISTRY_IMAGE}/agentk:${basetag}-amd64-race" \
+    --amend "${CI_REGISTRY_IMAGE}/agentk:${basetag}-arm-race" \
     --amend "${CI_REGISTRY_IMAGE}/agentk:${basetag}-arm64-race"
 
 docker manifest push "${CI_REGISTRY_IMAGE}/agentk:${basetag}-race"
