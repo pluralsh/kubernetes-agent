4d156cdb467b819bb773f628a921bcfa05661908
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2023-05-08 12:12:07 +1000

    Fix test flake

--- internal/module/reverse_tunnel/tracker/aggregating_querier_test.go
+++ internal/module/reverse_tunnel/tracker/aggregating_querier_test.go
@@ -113,8 +113,9 @@ func TestPollKasUrlsByAgentId_CacheWhenRunning(t *testing.T) {
 		q.EXPECT().
 			KasUrlsByAgentId(gomock.Any(), testhelpers.AgentId, gomock.Any()).
 			Do(func(ctx context.Context, agentId int64, cb tracker.KasUrlsByAgentIdCallback) {
-				close(start1) // start concurrent query
-				<-start2      // wait for the concurrent query to consume item from cache
+				close(start1)                      // start concurrent query
+				<-start2                           // wait for the concurrent query to consume item from cache
+				time.Sleep(100 * time.Millisecond) // wait for aq.PollKasUrlsByAgentId() to register second callback
 				_, err := cb("url2")
 				assert.NoError(t, err)
 			}),
