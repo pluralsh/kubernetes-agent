7752c8581a2236fc52a53d77763d54c9db5f26c5
Timo Furrer <tfurrer@gitlab.com>
2023-07-03 15:52:14 +0200

    Allow agent worker manager to be stopped and restarted

--- internal/module/modagent/worker_manager.go
+++ internal/module/modagent/worker_manager.go
@@ -111,11 +111,16 @@ func (m *WorkerManager[C]) ApplyConfiguration(agentId int64, cfg *agentcfg.Agent
 func (m *WorkerManager[C]) StopAllWorkers() {
 	// Tell all workers to stop
 	for _, holder := range m.workers {
+		m.log.Info("Stopping worker", logz.WorkerId(holder.sourceId))
 		holder.stop()
 	}
 	// Wait for all workers to stop
-	for _, holder := range m.workers {
+	for k, holder := range m.workers {
+		m.log.Info("Waiting for worker to stop", logz.WorkerId(holder.sourceId))
 		holder.wg.Wait()
+		// we clear the workers map to allow for a proper restart of the workers.
+		delete(m.workers, k)
+		m.log.Info("Stopped worker", logz.WorkerId(holder.sourceId))
 	}
 }
 
