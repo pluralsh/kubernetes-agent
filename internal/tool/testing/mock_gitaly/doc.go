2d2a93db5aedc852fbd5aca7f350c2f99726e594
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-10-20 10:36:29 +0200

    remove some mocks

--- internal/tool/testing/mock_gitaly/doc.go
+++
@@ -1,4 +0,0 @@
-// Mocks for Gitaly.
-package mock_gitaly
-
-//go:generate mockgen.sh -destination "gitaly.go" -package "mock_gitaly" "gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v16/internal/gitaly/vendored/gitalypb" "CommitServiceClient,CommitService_TreeEntryClient,SmartHTTPServiceClient,SmartHTTPService_InfoRefsUploadPackClient,CommitService_GetTreeEntriesClient"
--- internal/tool/testing/mock_usage_metrics/doc.go
+++
@@ -1,3 +0,0 @@
-package mock_usage_metrics
-
-//go:generate mockgen.sh -destination "api.go" "gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v16/internal/module/usage_metrics" "UsageTrackerInterface,Counter,UniqueCounter"
