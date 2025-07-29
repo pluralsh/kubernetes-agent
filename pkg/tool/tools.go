2ad369d9812fc27b0eacb99612ec2135b850a93c
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-07-29 11:36:01 +0200

    update golangci-lint

--- pkg/tool/tools.go
+++ pkg/tool/tools.go
@@ -4,7 +4,7 @@ package tool
 
 import (
 	_ "github.com/envoyproxy/protoc-gen-validate"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
